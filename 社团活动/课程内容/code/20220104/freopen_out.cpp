f4041d6d3f81b6c932cf753bc7fea4d204ad6f7a
zeerre <zeerre98988@gmail.com>
2022-01-05 14:03:27 +0800

    master_comp_20220105

---
+++ 社团活动/课程内容/code/20220104/freopen_out.cpp
@@ -0,0 +1,16 @@
+//*************************************************************************
+//* File Name: freopen_out.cpp
+//* Author:GeekBear
+//* Mail:zeerre98988@gmail.com 
+//* Created And Modified Time: ====2022-01-05  13-27-47====
+//*************************************************************************
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main() {
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+    
+    return 0;
+}
