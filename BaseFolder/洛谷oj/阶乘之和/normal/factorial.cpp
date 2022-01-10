7f8ee75cf7e8cb8d9f21cfe5177a5624e321f518
Zee.rre <zeerre98988@gmail.com>
2022-01-10 10:30:31 +0800

    Update factorial.cpp

--- BaseFolder/洛谷oj/阶乘之和/normal/factorial.cpp
+++ BaseFolder/洛谷oj/阶乘之和/normal/factorial.cpp
@@ -9,33 +9,26 @@
 #include<cstdio>
 using namespace std;
 int n,a[10000]={0},s[10000]={0};
-void change(int x)
-{
+void change(int x){
 	int g=0;
-	for(int i=10000;i>=0;i--)
-	{
+	for(int i=10000;i>=0;i--){
 		a[i]=a[i]*x+g;
 		g=a[i]/10;
 		a[i]=a[i]%10;
 	}
 }
-void qh()
-{
+void qh(){
 	int g=0;
-	for(int i=10000;i>=0;i--)
-	{
+	for(int i=10000;i>=0;i--){
 		s[i]=s[i]+a[i]+g;
 		g=s[i]/10;
 		s[i]=s[i]%10;
 	}
 }
-void sc()
-{
+void sc(){
 	int w;
-	for(int i=0;i<=10000;i++)
-	{
-		if(s[i]!=0)
-		{
+	for(int i=0;i<=10000;i++){
+		if(s[i]!=0){
 			w=i;
 			break;
 		}
@@ -43,12 +36,10 @@ void sc()
 	for(int i=w;i<=10000;i++)
 	   printf("%d",s[i]);
 }
-int main()
-{
+int main(){
 	scanf("%d",&n);
 	s[10000]=a[10000]=1;
-	for(int i=2;i<=n;i++)
-	{
+	for(int i=2;i<=n;i++){
 		change(i);
 		qh();
 	}
