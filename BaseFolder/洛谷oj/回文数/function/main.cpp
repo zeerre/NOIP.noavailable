b9cd55c99df43a9b5970429b0b0536bdeaaf0b09
zeerre <zeerre98988@gmail.com>
2022-01-11 16:27:46 +0800

    N_luogu_20220111

---
+++ BaseFolder/洛谷oj/回文数/function/main.cpp
@@ -0,0 +1,20 @@
+//*************************************************************************
+//* File Name: main.cpp
+//* Author:GeekBear
+//* Mail:zeerre98988@gmail.com 
+//* Created And Modified Time: ====2022-01-11  15-31-24====
+//*************************************************************************
+
+#include <bits/stdc++.h> 
+#include "palind.cpp"
+#define N 1000000
+
+using namespace std;
+int main() {
+    int n;
+    string s;
+    int q[], l, w[N], ans;
+    cin>>n>>s;
+    
+    return 0;
+}
---
+++ BaseFolder/洛谷oj/回文数/function/palind.h
@@ -0,0 +1,15 @@
+//*************************************************************************
+//* File Name: palind.h
+//* Author:GeekBear
+//* Mail:zeerre98988@gmail.com 
+//* Created And Modified Time: ====2022-01-11  15-33-33====
+//*************************************************************************
+
+#ifndef _PALIND_H
+#define _PALIND_H
+void add(int[], int[]);
+void turn(int,int[]);
+bool f(int[]);
+void init(int[],string);
+
+#endif
