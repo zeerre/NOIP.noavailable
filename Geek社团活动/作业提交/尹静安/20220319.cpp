a47852208c37ad824997de9c9b811b0ad2d5fdd1
TuratinZhii <zhiixiiang2021@163.com>
2022-03-20 08:12:34 -0700

    HermiteFunction_20220320

--- Geek社团活动/作业提交/尹静安/20220319.cpp
+++ Geek社团活动/作业提交/尹静安/20220319.cpp
@@ -5,9 +5,25 @@
 //* Created And Modified Time: ====2022-03-19  17-32-16====
 //*************************************************************************
 
-#include <iostream>
+#include"stdio.h"
 using namespace std;
-int main() {
-
+int ack(int,int);
+int main(){
+    int m,n;
+    printf("The Max num of m is 3 ; n is 10.\n");
+    scanf("%d %d",&m,&n);
+    printf("%d",ack(m,n));
     return 0;
+}
+
+int ack(int a,int b){   //The function is not overall.Think it.
+    if(a==0);
+        return b+1;
+    while(a>0){
+        if(b==0)
+            b=1;
+        else
+            b=ack(a,b-1);
+            a--;
+    }
 }
