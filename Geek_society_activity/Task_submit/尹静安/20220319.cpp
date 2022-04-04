c32618bfb0e4364d4e939a49018902e47056cd14
TuratinZhii <zhiixiiang@163.com>
2022-04-04 07:23:29 -0700

    20220404_Ackmann01

--- Geek_society_activity/Task_submit/尹静安/20220319.cpp
+++ Geek_society_activity/Task_submit/尹静安/20220319.cpp
@@ -5,9 +5,28 @@
 //* Created And Modified Time: ====2022-03-19  17-32-16====
 //*************************************************************************
 
-#include <iostream>
+#include"stdio.h"
 using namespace std;
-int main() {
-
+int ack(int,int);
+int main(){
+    int m,n,sum=0;
+    printf("The Max num of m is 3 ; n is 10.\n");
+    scanf("%d %d",&m,&n);
+    sum+=ack(m,n);
+    printf("%d",sum);
     return 0;
+}
+
+int ack(int a,int b){
+    if(a==0)
+        return b+1;
+    if(a!=0&&b==0){
+        b=1;
+        return ack(a,b);
+    }
+    if(a!=0&&b!=0){
+        b=ack(a,b-1);
+        a-=1;
+        return ack(a,b);
+    }
 }
