cfd8dba5c2cde3012f6554c409d2263c7978914f
TuratinZhii <zhiixiiang2021@163.com>
2022-04-04 18:43:15 -0700

    20220405_FinalSubmit

--- Geek_society_activity/Task_submit/尹静安/20220309.cpp
+++ Geek_society_activity/Task_submit/尹静安/20220309.cpp
@@ -5,9 +5,26 @@
 //* Created And Modified Time: ====2022-03-09  21-25-09====
 //*************************************************************************
 
-#include <iostream>
+//埃尔米特函数
+#include"stdio.h"
 using namespace std;
-int main() {
-
+int Hn(int,int);
+int main(){
+    printf("Please input x&n:\n");
+    int x,n,sum=0;
+    scanf("%d %d",&x,&n);
+    sum+=Hn(x,n);
+    printf("The result is:\n %d",sum);
     return 0;
+}
+
+int Hn(int x,int y){
+    int tem;
+    if(y==0)
+        tem=1;
+    if(y==1)
+        tem=2*x;
+    if(y>1)
+        tem=2*x*Hn(x,y-1)-2*(y-1)*Hn(x,y-2);
+    return tem;
 }
