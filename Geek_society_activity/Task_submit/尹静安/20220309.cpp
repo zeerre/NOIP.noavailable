db83eb4f2189cc3204fafad5542f19052619977b
zeerre <zeerre98988@gmail.com>
2022-04-05 11:04:58 +0800

    N_Modify

--- Geek_society_activity/Task_submit/尹静安/20220309.cpp
+++ Geek_society_activity/Task_submit/尹静安/20220309.cpp
@@ -8,6 +8,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-
+    cout<<"";
     return 0;
 }
--- Geek_society_activity/Task_submit/尹静安/20220319.cpp
+++ Geek_society_activity/Task_submit/尹静安/20220319.cpp
@@ -8,6 +8,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-
+    sss
     return 0;
 }
